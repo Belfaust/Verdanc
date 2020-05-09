2e65147faaaf56e2ea85728c4abb813e3a33c5c1
Belfaust <Belfaust@users.noreply.github.com>
2020-05-09 19:51:20 +0200

    mainbaseup

--- Assets/Scripts/Models/Buildings/MainBase.cs
+++ Assets/Scripts/Models/Buildings/MainBase.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class MainBase : MonoBehaviour
 {
-    public List<Sapling> InventorySaplingList;
+    public List<Sapling> InventorySaplingList = new List<Sapling>();
+    public List<Sapling> SaplingListToSell = new List<Sapling>();
     public void Selling(Sapling Saplingexample)
     {
         for (int i = 0; i < InventorySaplingList.Count; i++)
         {
             if (InventorySaplingList[i] == Saplingexample&& InventorySaplingList[i].Growth_State == 3)
             {
-            World_Controller._Instance.AddResources((int)(Saplingexample.Money_Base * (Saplingexample.Verdancy * 3) + Saplingexample.Fertility),0);
-            Saplingexample = null;       
+                SaplingListToSell.Add(Saplingexample);
+                Saplingexample = null;       
             }
         }
+    }
+    public void SendingSaplings()
+    {
+        for (int i = 0; i < SaplingListToSell.Count; i++)
+        {
+            World_Controller._Instance.AddResources((int)(SaplingListToSell[i].Money_Base*SaplingListToSell[i].Verdancy*3) + SaplingListToSell[i].Fertility,0);
+        }
+        SaplingListToSell = new List<Sapling>();
     }
 }
